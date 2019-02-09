f2876cb68d08b5ca22ec21ea18e26b889b73f4e6
Dylan Elliott <dylan.elliott@outware.com.au>
2019-02-09 19:29:38 +1100

    Add tvOS target

--- TraktKit.podspec
+++ TraktKit.podspec
@@ -9,6 +9,7 @@ Pod::Spec.new do |s|
   s.ios.deployment_target     = "10.0"
   s.osx.deployment_target     = "10.10"
   s.watchos.deployment_target = "3.0"
+  s.tvos.deployment_target = "11.0"
   s.source        = { :git => "https://github.com/MaxHasADHD/TraktKit.git", :tag => "#{s.version}" }
   s.source_files  = "Common", "Common/**/*.{h,m,swift}"
   s.requires_arc = true
---
+++ TraktKitTV/TraktKitTV.h
@@ -0,0 +1,19 @@
+//
+//  TraktKitTV.h
+//  TraktKitTV
+//
+//  Created by Dylan Elliott on 9/2/19.
+//  Copyright © 2019 Maximilian Litteral. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TraktKitTV.
+FOUNDATION_EXPORT double TraktKitTVVersionNumber;
+
+//! Project version string for TraktKitTV.
+FOUNDATION_EXPORT const unsigned char TraktKitTVVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TraktKitTV/PublicHeader.h>
+
+
